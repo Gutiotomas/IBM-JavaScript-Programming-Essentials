47d0d6e261ba82b5b2e152652a8b46ca322aecac
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-10-05 20:51:41 -0500

    Refactor health_article.html and health_article.js to add news articles section

--- Module_4/health_article.js
+++ Module_4/health_article.js
@@ -6,6 +6,7 @@ xhr.responseType = 'json';
 xhr.onload = function() {
 var articles = xhr.response.articles;
 var articlesDiv = document.getElementById('articles');
+  
 articles.forEach(function(article) {
     var articleDiv = document.createElement('div');
     articleDiv.classList.add('article');
@@ -48,3 +49,54 @@ articles.forEach(function(article) {
     articleDiv.classList.add('article');
   })};
   xhr.send();
+
+
+var xhr2 = new XMLHttpRequest();
+var url2 = 'news_article.json';
+xhr2.open('GET', url2, true);
+xhr2.responseType = 'json';
+
+xhr2.onload = function() {
+  var news = xhr2.response.articles;
+  var newsDiv = document.getElementById('news');
+
+  news.forEach(function(newsArticle) {
+    var newsArticleDiv = document.createElement('div');
+    newsArticleDiv.classList.add('news');
+
+    var title = document.createElement('h2');
+    title.textContent = newsArticle.title;
+
+    var description = document.createElement('p');
+    description.textContent = newsArticle.description;
+
+    var waysHeader = document.createElement('h3');
+    waysHeader.textContent = 'Ways to Achieve:';
+
+    var waysList = document.createElement('ul');
+    newsArticle.ways_to_achieve.forEach(function(way) {
+      var listItem = document.createElement('li');
+      listItem.textContent = way;
+      waysList.appendChild(listItem);
+    });
+
+    var benefitsHeader = document.createElement('h3');
+    benefitsHeader.textContent = 'Benefits:';
+
+    var benefitsList = document.createElement('ul');
+    newsArticle.benefits.forEach(function(benefit) {
+      var listItem = document.createElement('li');
+      listItem.textContent = benefit;
+      benefitsList.appendChild(listItem);
+    });
+
+    newsArticleDiv.appendChild(title);
+    newsArticleDiv.appendChild(description);
+    newsArticleDiv.appendChild(waysHeader);
+    newsArticleDiv.appendChild(waysList);
+    newsArticleDiv.appendChild(benefitsHeader);
+    newsArticleDiv.appendChild(benefitsList);
+    newsDiv.appendChild(newsArticleDiv);
+  });
+};
+xhr2.send();
