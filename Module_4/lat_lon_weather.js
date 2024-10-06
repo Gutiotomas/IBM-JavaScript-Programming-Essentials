66fd508c241b7621218e44a5b6647d18d693ca60
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-10-06 00:47:20 -0500

    Add lat_lon_weather.html and lat_lon_weather.js files for weather information retrieval from API

---
+++ Module_4/lat_lon_weather.js
@@ -0,0 +1,21 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
+    const apiKey = '9e95393ec241153e0a6107443f96e641'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+      });
+}
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
